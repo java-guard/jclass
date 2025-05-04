6b717a8e812a7f5ee7998094e485677973a8e1d4
kyle <kyle_derrick@foxmail.com>
2025-05-04 15:24:12 +0800

    lib 调整，去除main

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,12 @@
 #![allow(unused_attributes, unused, non_upper_case_globals)]
 
-mod common;
-mod classfile_constants;
-mod constant_pool;
-mod jclass_info;
+pub mod common;
+pub mod classfile_constants;
+pub mod constant_pool;
+pub mod jclass_info;
 mod util;
-mod field_info;
-mod attribute_info;
-mod method_info;
+pub mod field_info;
+pub mod attribute_info;
+pub mod method_info;
 mod support;
-
-#[cfg(test)]
-mod tests {
-    use std::fs::{read, File};
-    use std::io::{BufReader, Cursor};
-    use std::time::Instant;
-    use crate::jclass_info::JClassInfo;
-
-    #[test]
-    fn test_parser() {
-        let file_path = "D:\\data\\code\\idea\\test-all\\target\\classes\\cn\\kyle\\test\\all\\base\\HutoolScriptTest.class";
-        let file_path = "D:\\data\\code\\project\\JavaGuard\\JavaGuard\\target\\classes\\javassist\\bytecode\\ClassDecryption.class";
-        let content = File::open(file_path).unwrap();
-        let now = Instant::now();
-        let mut  info = JClassInfo::from_reader(&mut content.into());
-        println!(">> {:?}", now.elapsed().as_nanos());
-        if let Ok(info) = info {
-            println!("{:?}", &info);
-        }
-
-        let content = read(file_path).unwrap();
-        let mut t = 0;
-        for _ in 0..10000 {
-            // let content_ref = content.clone();
-            // let cursor = Cursor::new(content_ref);
-            let cursor = Cursor::new(&content);
-            let now = Instant::now();
-            let mut _info = JClassInfo::from_reader(&mut cursor.into());
-            let duration = now.elapsed();
-            t += duration.as_nanos();
-        }
-        println!(">> {:?}", t);
-    }
-
-    #[test]
-    fn test_parser_step() {
-        // let file_path = "D:\\data\\code\\idea\\test-all\\target\\classes\\cn\\kyle\\test\\all\\base\\HutoolScriptTest.class";
-        let file_path = "D:\\data\\code\\project\\JavaGuard\\JavaGuard\\target\\classes\\javassist\\bytecode\\ClassDecryption.class";
-        let content = File::open(file_path).unwrap();
-
-        let mut total = 0;
-        let now = Instant::now();
-        let mut  info = JClassInfo::from_reader(&mut BufReader::new(content).into());
-        let duration = now.elapsed();
-        total += duration.as_nanos();
-        println!(">> total: {:?}", duration);
-    }
-}
 
--- tests/base_test.rs
+++ tests/base_test.rs
@@ -1,26 +1,15 @@
-#![allow(unused_attributes, unused, non_upper_case_globals)]
-
-mod common;
-mod classfile_constants;
-mod constant_pool;
-mod jclass_info;
-mod util;
-mod field_info;
-mod attribute_info;
-mod method_info;
-mod support;
-
 use std::cmp::{max, min};
 use std::collections::HashSet;
 use std::fs::{read, File};
 use std::io::{BufReader, Cursor};
 use std::time::Instant;
-use crate::attribute_info::CodeAttribute;
-use crate::common::constants::CODE_TAG;
-use crate::constant_pool::ConstantValue;
-use crate::jclass_info::JClassInfo;
+use jclass::jclass_info::JClassInfo;
+use jclass::attribute_info::CodeAttribute;
+use jclass::common::constants::CODE_TAG;
+use jclass::constant_pool::ConstantValue;
 
-fn main() {
+#[test]
+fn base_test() {
     let file_path = "D:\\data\\code\\idea\\test-all\\target\\classes\\cn\\kyle\\test\\all\\base\\HutoolScriptTest.class";
     let file_path = "D:\\data\\code\\project\\JavaGuard\\JavaGuard\\target\\classes\\javassist\\bytecode\\ClassDecryption.class";
     let content = File::open(file_path).unwrap();
@@ -92,4 +81,30 @@ fn main() {
     println!(">> min: {:?}", min_t);
     println!(">> max: {:?}", max_t);
     println!(">> avg: {:?}", avg_t/10000);
+}
+
+#[test]
+fn test_parser() {
+    let file_path = "D:\\data\\code\\idea\\test-all\\target\\classes\\cn\\kyle\\test\\all\\base\\HutoolScriptTest.class";
+    let file_path = "D:\\data\\code\\project\\JavaGuard\\JavaGuard\\target\\classes\\javassist\\bytecode\\ClassDecryption.class";
+    let content = File::open(file_path).unwrap();
+    let now = Instant::now();
+    let mut  info = JClassInfo::from_reader(&mut content.into());
+    println!(">> {:?}", now.elapsed().as_nanos());
+    if let Ok(info) = info {
+        println!("{:?}", &info);
+    }
+
+    let content = read(file_path).unwrap();
+    let mut t = 0;
+    for _ in 0..10000 {
+        // let content_ref = content.clone();
+        // let cursor = Cursor::new(content_ref);
+        let cursor = Cursor::new(&content);
+        let now = Instant::now();
+        let mut _info = JClassInfo::from_reader(&mut cursor.into());
+        let duration = now.elapsed();
+        t += duration.as_nanos();
+    }
+    println!(">> {:?}", t);
 }
